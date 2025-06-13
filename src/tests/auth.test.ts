3aec1ad0c49d165050ad0d801628cec91f2f1044
Isaac Schubert <greenace14@gmail.com>
2025-06-13 12:12:59 +0700

    wip

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.ts";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
+
+describe("auth test", () => {
+  test("not defined", () => {
+    expect(getAPIKey({})).toBe(null)
+  });
+
+});
+
+
